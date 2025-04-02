d64a03b2d0d94daf1fc1825524034acf09c056a4
ealeo <e.a.leonard@icloud.com>
2025-04-02 15:18:51 +1000

    commit

---
+++ src/main/java/com/example/cab302week4/model/MockContactDAO.java
@@ -0,0 +1,55 @@
+package com.example.cab302week4.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockContactDAO implements IContactDAO {
+    /**
+     * A static list of contacts to be used as a mock database.
+     */
+    public static final ArrayList<Contact> contacts = new ArrayList<>();
+    private static int autoIncrementedId = 0;
+
+    public MockContactDAO() {
+        // Add some initial contacts to the mock database
+        addContact(new Contact("John", "Doe", "johndoe@example.com", "0423423423"));
+        addContact(new Contact("Jane", "Doe", "janedoe@example.com", "0423423424"));
+        addContact(new Contact("Jay", "Doe", "jaydoe@example.com", "0423423425"));
+
+    }
+
+
+    public void addContact(Contact contact) {
+        contact.setId(autoIncrementedId);
+        autoIncrementedId++;
+        contacts.add(contact);
+    }
+
+
+    public void updateContact(Contact contact) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getId() == contact.getId()) {
+                contacts.set(i, contact);
+                break;
+            }
+        }
+    }
+
+    public void deleteContact(Contact contact) {
+        contacts.remove(contact);
+    }
+
+
+    public Contact getContact(int id) {
+        for (Contact contact : contacts) {
+            if (contact.getId() == id) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public List<Contact> getAllContacts() {
+        return new ArrayList<>(contacts);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,13 @@ module com.example.cab302week4 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens com.example.cab302week4 to javafx.fxml;
     exports com.example.cab302week4;
+    exports com.example.cab302week4.controller;
+    opens com.example.cab302week4.controller to javafx.fxml;
+    exports com.example.cab302week4.model;
+    opens com.example.cab302week4.model to javafx.fxml;
 }
