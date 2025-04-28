12b5e0d7b6753ce42542c6ba803c88d9f04d45cd
OliviaMiley <n11558555@qut.edu.au>
2025-04-28 14:18:27 +1000

    Reg and login pages

---
+++ src/main/java/com/example/cab302week4/controller/LoginController.java
@@ -0,0 +1,47 @@
+package com.example.cab302week4.controller;
+
+import com.example.cab302week4.model.IAlfredDAO;
+import com.example.cab302week4.model.SqliteAlfredDAO;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+
+
+public class LoginController {
+    private SqliteAlfredDAO alfredDAO;
+    public int LoggedinUserID;
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label messageLabel;
+
+
+
+    @FXML
+    private void initialize() {
+        alfredDAO = new SqliteAlfredDAO();
+    }
+
+    @FXML
+    private void handleLogin(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        System.out.println(username);
+        System.out.println(password);
+        int FindUser = alfredDAO.getUserID(username,password);
+
+        if(FindUser == 0){
+            messageLabel.setText("This combination of username and password is invalid");
+        }else{
+            LoggedinUserID = FindUser;
+            messageLabel.setText("You have sucessfully logged in");
+        }
+    }
+}
