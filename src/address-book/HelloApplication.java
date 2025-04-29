0dd4016ea2dd3f566abe839c2bb1c44fa3173da4
lmraad <lmraad55@gmail.com>
2025-04-30 09:15:24 +1000

    Added address-book files to new folder

---
+++ src/address-book/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.cab302week4;
+
+import com.example.cab302week4.model.Quiz;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    // Constants defining the window title and size
+    public static final String TITLE = "Alfred AI";
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 360;
+    public static Quiz quiz;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
